b2e16703a161effb14a605031784dbffe81ad310
Sonna <Alex.Sonneveld@live.com.au>
2013-09-14 15:24:59 +1000

    Create new "Blog" Rails Application
    - Commits a basic generated Rails Application called "Blog"
    
    - It is shared, along with the project files, into the Vagrant Virtual
    Machine

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c2b424a9044573be057cd9695f6f2c6fac1309a2bab823a739fb55d8583bf5d8ae6f71cd82ae46a7b86e57a06c0a606d130c55960069923086ee7e9b5ca91de4'
